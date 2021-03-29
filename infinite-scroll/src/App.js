a11079248a615b1c89fd901b441d7a9c3b6174ac
Arumuga priya <arumugapriya@smitiv.co>
2021-03-29 13:16:43 +0530

    initial commit

---
+++ infinite-scroll/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import InfiniteSpace from "./InfiniteSpace";
+
+function App() {
+  return (
+    <div className="App">
+      <InfiniteSpace />
+    </div>
+  );
+}
+
+export default App;
